82bff0d1c1b35815165aae543085438410b447c8
Elisa Demanez <demanez.elisa@gmail.com>
2018-02-28 16:34:54 +0100

    refacto

--- article.php
+++ article.php
@@ -1,22 +1,16 @@
 <?php
-
-
-$id = ($_GET['id']);
+$article_id = ($_GET['id']);
             
-$article = $connection->query("SELECT * FROM `blog_article` WHERE id= $id" )->fetch_assoc();
+$article = $connection->query("SELECT * FROM `blog_article` WHERE id= $article_id" )->fetch_assoc();
 
 ?> 
 <div class="blog">
-<h2>
-        <?php print $article['titre']; ?>
-</h2>
+        
+<h2>  <?php print $article['titre']; ?> </h2>
 
-<p class="date"> 
-        <?php print $article['date']; ?>
-</p>
+<p class="date">  <?php print $article['date']; ?> </p>
+
 <img src="images/<?php print $article['image']; ?>" alt="the article picture" class="img">
 
-<p>
-<?php print $article['texte']; ?>
-</p>
+<p> <?php print $article['texte']; ?> </p>
 </div>
