dddfb22dfc6ada5982420994dc1a9ea658141734
Elisa Demanez <demanez.elisa@gmail.com>
2018-03-01 09:45:19 +0100

    working

---
+++ components/delete_article.php
@@ -0,0 +1,12 @@
+<?php echo 'bjr';
+var_dump($_GET);
+$article_id = $_GET['article_id'];
+
+$sql = "DELETE FROM `blog_article` WHERE `id` = $article_id ";
+        
+if (mysqli_query($connection, $sql)) {
+    header('Location: index.php?selected=blog');
+} else {
+    echo "Error: ";
+}
+?>
